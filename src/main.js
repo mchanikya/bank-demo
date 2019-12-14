45917e224a7b853cf04e91cc7d6ac0723090378a
chanikya <chanikya.prakash@gmail.com>
2019-12-14 16:56:39 +0530

    added header submenu and bank login page

---
+++ src/main.js
@@ -0,0 +1,15 @@
+(function(){
+  'use strict';
+
+  angular.module('BankAppDemo',[])
+  .component('bankHeader',{
+    templateUrl:'templates/bank-header.template.html'
+  })
+  .component('bankSubMenu',{
+    templateUrl:'templates/bank-sub-menu.template.html'
+  })
+  .component('bankBody',{
+    templateUrl:'templates/bank-body.template.html'
+  });
+
+})();
