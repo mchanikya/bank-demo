47d6016b85928243d7424d60244a14cd800b951e
chanikya <chanikya.prakash@gmail.com>
2019-12-15 17:38:22 +0530

    Introduced config file

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 
   angular.module('BankAppDemo',[])
   .component('bankHeader',{
-    templateUrl:'templates/bank-header.template.html'
+    templateUrl:'templates/bank-header.template.html',
+    controller:'headerController as hCtrl'
   })
+  .controller('headerController',headerController)
   .component('bankSubMenu',{
     templateUrl:'templates/bank-sub-menu.template.html'
   })
@@ -13,6 +15,34 @@
   })
   .component('bankBody',{
     templateUrl:'templates/bank-body.template.html'
-  });
+  })
+  .service('configService',configService);
+
+  headerController.$inject=['configService']
+  function headerController(configService){
+    var $ctrl=this;
+    $ctrl.$onInit=function(){
+      configService.getCfgDetails();
+    };
+  }
+
+  configService.$inject=['$http'];
+  function configService($http) {
+    var service=this;
+    service.configDetails=[];
+
+    service.getCfgDetails=function(){
+      return service.configDetails;
+    };
+
+    service.promise=$http.get('config/website-details.json');
+    service.promise.then(function(res){
+      console.log(res);
+      service.configDetails=res.data;
+    })
+    .catch(function() {
+      console.log("Failed to read configuration details");
+    });
+  }
 
 })();
