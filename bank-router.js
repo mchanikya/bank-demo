316a45a447b49a8c0ed5d7017bcf3de173e48299
chanikya <chanikya.prakash@gmail.com>
2020-01-19 23:17:56 +0530

    Added routing to sub menu items and user login help

--- bank-router.js
+++ bank-router.js
@@ -6,16 +6,44 @@
 
 	RoutesConfig.$inject=['$stateProvider','$urlRouterProvider'];
 	function RoutesConfig($stateProvider,$urlRouterProvider){
-		$urlRouterProvider.otherwise('/');
+		$urlRouterProvider.otherwise('home');
 
 		$stateProvider
-		.state('/',{
-			url: '/',
-			templateUrl: 'index.html'
+		.state('home',{
+			url: '/home',
+			templateUrl: 'templates/home.html'
+		})
+		.state('NRI',{
+			url: '/NRI',
+			templateUrl: 'templates/nri.html'
+		})
+		.state('Personal',{
+			url: '/Personal',
+			templateUrl: 'templates/user-login.template.html'
+		})
+		.state('Business',{
+			url: '/Business',
+			templateUrl: 'templates/business.html'
 		})
 		.state('accountSummary',{
 			url: '/accountSummary',
 			templateUrl: 'templates/user-account.template.html'
+		})
+		.state('getUserId',{
+			url:"/getUserId",
+			templateUrl:"templates/get-user.template.html"
+		})
+		.state('getPassword',{
+			url:"/getPassword",
+			templateUrl:"templates/get-password.template.html"
+		})
+		.state('TroubleLogging',{
+			url:"/TroubleLogging",
+			templateUrl:"templates/trouble-logging.template.html"
+		})
+		.state('OnlineDemo',{
+			url:"/OnlineDemo",
+			templateUrl:"templates/online-demo.template.html"
 		});
 	}
 
